20e8b9c1ca4563475e5e0e6e3187e6dff6d1eab0
Diego Aldao <diego_ind@live.com.ar>
2023-07-14 18:17:26 -0300

    añadido el componente que renderiza la pagina principal, con los componentes de la seccion Main

---
+++ src/pages/PaginaInicio.tsx
@@ -0,0 +1,24 @@
+import MainLayout from "../layout/MainLayout";
+import personaje from "../assets/personaje.png";
+import animeWallpaper from "../assets/anime.jpg";
+import MainTitulo from "../components/PaginaInicio/MainSection/MainTitulo";
+import MainCardPersonaje from "../components/PaginaInicio/MainSection/MainCardPersonaje";
+import MainCardAnime from "../components/PaginaInicio/MainSection/MainCardAnime";
+import MainButtonRandom from "../components/PaginaInicio/MainSection/MainButtonRandom";
+import MainInfoApp from "../components/PaginaInicio/MainSection/MainInfoApp";
+
+const PaginaInicio = () => {
+  return (
+    <MainLayout>
+      <section className="main_section">
+        <MainTitulo texto={"odyssey: comunidad anime"} />
+        <MainCardPersonaje personaje={personaje} />
+        <MainButtonRandom />
+        <MainCardAnime animeWallpaper={animeWallpaper} />
+        <MainInfoApp />
+      </section>
+    </MainLayout>
+  );
+};
+
+export default PaginaInicio;
