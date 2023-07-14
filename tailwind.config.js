fc6f2f7cfd695f3049e13984e2a182b3a187cf1f
Diego Aldao <diego_ind@live.com.ar>
2023-07-14 18:08:48 -0300

    agregado un color secundario de background, dos tamaños de screens y una animacion de spin mas lenta

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ export default {
     extend: {
       colors: {
         "main-color-background": "#e6e4d5",
+        "secondary-color-background": "#e7e3ce",
         "main-black": "#23210f",
         "main-white": "#efefef",
         "main-color": "#fcff60",
@@ -14,6 +15,10 @@ export default {
       },
       screens: {
         xl: "1240px",
+        xs: "580px",
+      },
+      animation: {
+        "spin-slow": "spin 5s linear infinite",
       },
     },
   },
