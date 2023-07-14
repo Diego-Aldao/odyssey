b5f0fc6dc76ab029043194334511ca0b001a428e
Diego Aldao <diego_ind@live.com.ar>
2023-07-14 18:09:47 -0300

    agregado un contenedor de la app y el componente PaginaInicio, por ahora sin router hasta que cree las demas paginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import PaginaInicio from "./pages/PaginaInicio";
+
 function App() {
-  return <></>;
+  return (
+    <div className="bg-main-black w-full border-4 rounded-xl max-w-screen-2xl mx-auto  border-main-color-background p-3">
+      <PaginaInicio />
+    </div>
+  );
 }
 
 export default App;
