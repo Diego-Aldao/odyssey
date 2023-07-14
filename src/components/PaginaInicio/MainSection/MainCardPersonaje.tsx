8475b87bd4522f5fde2487ef17c9f8a534aa70f3
Diego Aldao <diego_ind@live.com.ar>
2023-07-14 18:15:53 -0300

    añadido el componente que hace el fetch a random personaje

---
+++ src/components/PaginaInicio/MainSection/MainCardPersonaje.tsx
@@ -0,0 +1,32 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
+
+type Props = {
+  personaje: string;
+};
+
+const MainCardPersonaje: React.FC<Props> = ({ personaje }) => {
+  return (
+    <div className="main_card_personaje">
+      <div className=" w-10/12 h-full pl-5 pt-5">
+        <img
+          src={personaje}
+          alt="imagen de un personaje de anime"
+          className="object-fill"
+        />
+      </div>
+      <div className="h-full w-2/12 bg-main-color flex flex-col justify-between py-4 items-center border-l-[1.5px] border-main-black overflow-hidden  rounded-r-[20px]">
+        <div className="w-full flex flex-col items-center gap-4">
+          <Icon icon="game-icons:beveled-star" className="h-6 w-6" />
+          <Icon icon="game-icons:beveled-star" className="h-6 w-6" />
+        </div>
+
+        <Icon
+          icon="game-icons:branch-arrow"
+          className="h-8 w-8 -rotate-[135deg]"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default MainCardPersonaje;
