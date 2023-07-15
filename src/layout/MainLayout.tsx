9fbe38889ca4b04867567262236e455a877b5fe2
Diego Aldao <diego_ind@live.com.ar>
2023-07-15 17:56:26 -0300

    añadida la layout principal de las paginas

---
+++ src/layout/MainLayout.tsx
@@ -0,0 +1,22 @@
+import BotonLogin from "../components/Layout/BotonLogin";
+import IconoHeader from "../components/Layout/IconoHeader";
+import NavegacionPrincipal from "../components/NavegacionPrincipal/NavegacionPrincipal";
+
+type Props = {
+  children: JSX.Element;
+};
+
+const MainLayout: React.FC<Props> = ({ children }) => {
+  return (
+    <>
+      <header className="bg-main-color-background rounded-t-2xl flex items-center h-14 justify-between pl-4 md:px-6 lg:px-10">
+        <IconoHeader />
+        <NavegacionPrincipal />
+        <BotonLogin />
+      </header>
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default MainLayout;
