36a4c657ef074735eb213960e7c5ffe6d34bdc7f
Diego Aldao <diego_ind@live.com.ar>
2023-07-15 17:54:38 -0300

    añadidos estilos para screen xs

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import PaginaInicio from "./pages/PaginaInicio";
 
 function App() {
   return (
-    <div className="bg-main-black w-full border-4 rounded-xl max-w-screen-2xl mx-auto  border-main-color-background p-3">
+    <div className="bg-main-black w-full xs:border-4 rounded-xl max-w-screen-2xl mx-auto  border-main-color-background xs:p-3">
       <PaginaInicio />
     </div>
   );
