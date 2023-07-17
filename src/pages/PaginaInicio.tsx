c03710f79ae0fb3a90d58cbeb9225af2e1bcd369
Diego Aldao <diego_ind@live.com.ar>
2023-07-17 14:13:58 -0300

    añadido el header de la segunda seccion

--- src/pages/PaginaInicio.tsx
+++ src/pages/PaginaInicio.tsx
@@ -6,17 +6,23 @@ import MainCardPersonaje from "../components/PaginaInicio/MainSection/MainCardPe
 import MainCardAnime from "../components/PaginaInicio/MainSection/MainCardAnime";
 import MainButtonRandom from "../components/PaginaInicio/MainSection/MainButtonRandom";
 import MainInfoApp from "../components/PaginaInicio/MainSection/MainInfoApp";
+import SubHeader from "../components/PaginaInicio/SubSections/SubHeader";
 
 const PaginaInicio = () => {
   return (
     <MainLayout>
-      <section className="main_section">
-        <MainTitulo texto={"odyssey: comunidad anime"} />
-        <MainCardPersonaje personaje={personaje} />
-        <MainButtonRandom />
-        <MainCardAnime animeWallpaper={animeWallpaper} />
-        <MainInfoApp />
-      </section>
+      <>
+        <section className="main_section">
+          <MainTitulo texto={"odyssey: comunidad anime"} />
+          <MainCardPersonaje personaje={personaje} />
+          <MainButtonRandom />
+          <MainCardAnime animeWallpaper={animeWallpaper} />
+          <MainInfoApp />
+        </section>
+        <section className="sub_section w-full">
+          <SubHeader titulo="anime de temporada" />
+        </section>
+      </>
     </MainLayout>
   );
 };
