ff62df0c4bd11ed2da7482895ac98e66062c4a01
Diego Aldao <diego_ind@live.com.ar>
2023-07-17 14:13:27 -0300

    añadido el componente con el boton login

---
+++ src/components/Layout/BotonLogin.tsx
@@ -0,0 +1,11 @@
+const BotonLogin = () => {
+  return (
+    <div className="hidden md:flex items-center justify-end h-full w-1/6 lg:w-1/5">
+      <button className="bg-main-white px-6 capitalize py-1 rounded-2xl lg:px-10 border-main-black border-[1px] font-semibold text-sm lg:text-base">
+        login
+      </button>
+    </div>
+  );
+};
+
+export default BotonLogin;
