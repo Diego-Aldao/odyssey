49872da48a79f1bae256363ff8b7a987e0278c50
Diego Aldao <diego_ind@live.com.ar>
2023-08-03 19:07:04 -0300

    añadido el browser router con el array de paths con sus elementos y el router provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
+import PaginaEpisodios from "./pages/PaginaEpisodios";
 import PaginaInicio from "./pages/PaginaInicio";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import PaginaTemporadas from "./pages/PaginaTemporadas";
+import PaginaTrailers from "./pages/PaginaTrailers";
+import PaginaTops from "./pages/PaginaTops";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <PaginaInicio />,
+  },
+  {
+    path: "/episodios",
+    element: <PaginaEpisodios />,
+  },
+  {
+    path: "/temporadas",
+    element: <PaginaTemporadas />,
+  },
+  {
+    path: "/trailers",
+    element: <PaginaTrailers />,
+  },
+  {
+    path: "/tops",
+    element: <PaginaTops />,
+  },
+]);
 
 function App() {
   return (
     <div className="bg-main-black w-full xs:border-4 rounded-xl max-w-screen-2xl mx-auto  border-main-color-background xs:p-3">
-      <PaginaInicio />
+      <RouterProvider router={router} />
     </div>
   );
 }
