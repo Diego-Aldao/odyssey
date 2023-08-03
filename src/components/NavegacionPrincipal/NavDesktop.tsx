506cac5fdc5f907e07f91718655f47dfae89fe43
Diego Aldao <diego_ind@live.com.ar>
2023-08-03 19:11:09 -0300

    añadida la navegacion desktop aun sin la funcionalidad de la barra de busqueda

---
+++ src/components/NavegacionPrincipal/NavDesktop.tsx
@@ -0,0 +1,37 @@
+import { Icon } from "@iconify/react";
+import { Link } from "react-router-dom";
+
+type Props = {
+  texto: string;
+  destino: string;
+};
+
+const LinkNavegacion: React.FC<Props> = ({ texto, destino }) => {
+  return (
+    <li>
+      <Link
+        to={destino}
+        className="capitalize text-[13px] lg:text-base flex gap-2 items-center"
+      >
+        {texto}
+      </Link>
+    </li>
+  );
+};
+
+const NavDesktop = () => {
+  return (
+    <ul className="hidden md:flex h-full items-center justify-center gap-4 lg:gap-3 xl:gap-6">
+      <LinkNavegacion destino="/" texto="inicio" />
+      <LinkNavegacion destino="/episodios" texto="ultimos episodios" />
+      <LinkNavegacion destino="/temporadas" texto="anime de temporada" />
+      <LinkNavegacion destino="/tops" texto="tops" />
+      <li className="capitalize text-[13px] lg:text-base flex gap-2 items-center">
+        <span className="hidden xl:block">buscar</span>
+        <Icon icon="mingcute:search-3-line" className="h-4 w-4 lg:h-5 lg:w-5" />
+      </li>
+    </ul>
+  );
+};
+
+export default NavDesktop;
