a8ae050f5b0f519ad6f9e187fcaaf95afd0eb21d
Diego Aldao <diego_ind@live.com.ar>
2023-08-05 20:27:23 -0300

    agregado archivo con constantes

---
+++ src/constants.ts
@@ -0,0 +1,120 @@
+export const MAIN_FILTERS_SEASONS = [
+  {
+    nombre: "verano 2023",
+    query: "/now",
+    id: 1,
+  },
+  {
+    nombre: "otoño 2023",
+    query: "/2023/fall",
+    id: 2,
+  },
+  {
+    nombre: "invierno 2023",
+    query: "/2023/winter",
+    id: 3,
+  },
+];
+
+export const SECONDARY_FILTERS_SEASONS = [
+  {
+    nombre: "todos",
+    query: "",
+    id: 1,
+  },
+  {
+    nombre: "tv",
+    query: "tv",
+    id: 2,
+  },
+  {
+    nombre: "ona",
+    query: "ona",
+    id: 3,
+  },
+  {
+    nombre: "ova",
+    query: "ova",
+    id: 4,
+  },
+  {
+    nombre: "pelicula",
+    query: "movie",
+    id: 5,
+  },
+  {
+    nombre: "especial",
+    query: "special",
+    id: 6,
+  },
+];
+
+export const MAIN_FILTERS_TRAILERS = [
+  {
+    nombre: "recientes",
+    query: "",
+    id: 1,
+  },
+  {
+    nombre: "populares",
+    query: "/popular",
+    id: 2,
+  },
+];
+
+export const MAIN_FILTERS_TOPS = [
+  {
+    nombre: "animes",
+    query: "/anime",
+    id: 1,
+  },
+  {
+    nombre: "personajes",
+    query: "/characters",
+    id: 2,
+  },
+  {
+    nombre: "personas",
+    query: "/people",
+    id: 3,
+  },
+];
+
+export const SECONDARY_FILTERS_TOPS = [
+  {
+    nombre: "todos",
+    query: "",
+    id: 1,
+  },
+  {
+    nombre: "tv",
+    query: "tv",
+    id: 2,
+  },
+  {
+    nombre: "ona",
+    query: "ona",
+    id: 3,
+  },
+  {
+    nombre: "ova",
+    query: "ova",
+    id: 4,
+  },
+  {
+    nombre: "pelicula",
+    query: "movie",
+    id: 5,
+  },
+  {
+    nombre: "especial",
+    query: "special",
+    id: 6,
+  },
+];
+
+export const BASE_URL_EPISODES = "https://api.jikan.moe/v4/watch/episodes";
+
+export const BASE_URL_SEASONS = "https://api.jikan.moe/v4/seasons";
+
+export const BASE_URL_TRAILERS = "https://api.jikan.moe/v4/watch/promos";
