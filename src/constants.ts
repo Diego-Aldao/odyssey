68c5434d59956b9c0abca54da783840ba6776da8
Diego Aldao <diego_ind@live.com.ar>
2023-08-16 19:54:14 -0300

    añadida la constante de los filtros main de episodios y de la url base de detalle

--- src/constants.ts
+++ src/constants.ts
@@ -16,6 +16,19 @@ export const MAIN_FILTERS_SEASONS = [
   },
 ];
 
+export const MAIN_FILTERS_EPISODES = [
+  {
+    nombre: "recientes",
+    query: "",
+    id: 1,
+  },
+  {
+    nombre: "populares",
+    query: "/popular",
+    id: 2,
+  },
+];
+
 export const SECONDARY_FILTERS_SEASONS = [
   {
     nombre: "todos",
@@ -118,3 +131,5 @@ export const BASE_URL_EPISODES = "https://api.jikan.moe/v4/watch/episodes";
 export const BASE_URL_SEASONS = "https://api.jikan.moe/v4/seasons";
 
 export const BASE_URL_TRAILERS = "https://api.jikan.moe/v4/watch/promos";
+
+export const BASE_URL_DETAILS = "https://api.jikan.moe/v4/anime";
