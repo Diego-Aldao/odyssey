460ba3824d8910e4db0534eac1015bc6e77d04dd
Diego Aldao <diego_ind@live.com.ar>
2023-08-18 18:57:26 -0300

    añadido el componente que renderiza la informacion especifica sobre el anime

---
+++ src/components/PaginaDetalle/AsideInfo.tsx
@@ -0,0 +1,53 @@
+import { ObjetoMal } from "../../types";
+
+type Info = {
+  nombre: string;
+  data: string | number | never[] | ObjetoMal[] | undefined;
+};
+
+type Props = {
+  imagenUrl: string;
+  data: Info[];
+};
+
+const AsideInfo: React.FC<Props> = ({ imagenUrl, data }) => {
+  return (
+    <aside className="hidden md:flex flex-col gap-10 z-20 relative col-start-1 row-start-1  w-full">
+      <div className="border-2 rounded-xl overflow-hidden border-main-black  w-full">
+        <img src={imagenUrl} alt="" />
+      </div>
+      <div className="info">
+        <div className="border-b-[3px] border-main-black w-[calc(100%_+_14px)] lg:w-[calc(100%_+_18px)]">
+          <p className="subtitulo-aside-detalle relative  capitalize text-lg inline-block text-main-color-background bg-main-black px-2 rounded-t-md rounded-tr-[10px]">
+            información
+          </p>
+        </div>
+        <ul className="flex flex-col gap-2 my-5">
+          {data.map((item) => (
+            <>
+              {!Array.isArray(item.data) ? (
+                <li>
+                  <span className="capitalize">{item.nombre}: </span>
+                  <span>{item.data}</span>
+                </li>
+              ) : (
+                <>
+                  {item.data.length >= 1 && (
+                    <li>
+                      <span className="capitalize">{item.nombre}: </span>
+                      {item.data.map((obj) => (
+                        <span key={obj.mal_id}>{obj.name}</span>
+                      ))}
+                    </li>
+                  )}
+                </>
+              )}
+            </>
+          ))}
+        </ul>
+      </div>
+    </aside>
+  );
+};
+
+export default AsideInfo;
