2f52b644d55ec35c0bf4601de8a5bee67a02fc8d
Diego Aldao <diego_ind@live.com.ar>
2023-08-22 19:46:18 -0300

    agregado el type Props, el custom hook useFetch, modificada la forma de guardar la currentData y agregado conditional rendering para renderizar o no el componente dependiendo del estado loading y currentData

--- src/components/PaginaDetalle/Episodios/EpisodiosDetalle.tsx
+++ src/components/PaginaDetalle/Episodios/EpisodiosDetalle.tsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react";
 import SectionDetalle from "../SectionDetalle";
-import useFetchSecondary from "../../../hooks/useFetchSecondary";
 import { BASE_URL_DETAILS } from "../../../constants";
-import { DataEpisode } from "../../../types";
+import { ApiResponseDetalleEpisodios, DataEpisode } from "../../../types";
 import { useParams } from "react-router-dom";
+import useFetch from "../../../hooks/useFetch";
 
-const EpisodiosDetalle = () => {
+type Props = {
+  visibleContent?: string;
+};
+
+const EpisodiosDetalle = ({ visibleContent }: Props) => {
   const [currentData, setCurrentData] = useState<DataEpisode[]>();
-  const { fetchData, data, loading } = useFetchSecondary();
+  const { fetchData, data, loading } = useFetch<ApiResponseDetalleEpisodios>();
   const { id } = useParams();
 
   useEffect(() => {
@@ -17,31 +21,43 @@ const EpisodiosDetalle = () => {
 
   useEffect(() => {
     if (!data) return;
-
-    setCurrentData(data.data.slice(0, 4));
-  }, [data]);
+    const newData = data?.data.slice(0, 4);
+    if (visibleContent === "episodios") {
+      setCurrentData(data.data);
+    } else {
+      setCurrentData(newData);
+    }
+  }, [data, visibleContent]);
 
   return (
-    <SectionDetalle titulo="episodios">
-      {loading ? (
+    <>
+      {loading || !currentData ? (
         <>cargando</>
       ) : (
-        <div className="grid sm:grid-cols-2 gap-2 lg:grid-cols-4">
-          {currentData?.map((episodio) => (
-            <article
-              key={episodio.mal_id}
-              className="mb-5 flex flex-col max-w-sm mx-auto w-full"
-            >
-              <p className="capitalize">episodio {episodio.mal_id}</p>
-              <div className="rounded-xl overflow-hidden border-2 border-main-black my-1">
-                <img src={episodio.images.jpg.image_url} alt="" />
-              </div>
-              <p className="self-end">{episodio.title}</p>
-            </article>
-          ))}
-        </div>
+        <>
+          {currentData.length >= 1 &&
+            (visibleContent === "general" ||
+              visibleContent === "episodios") && (
+              <SectionDetalle titulo="episodios">
+                <div className="grid sm:grid-cols-2 gap-2 lg:grid-cols-4">
+                  {currentData?.map((episodio) => (
+                    <article
+                      key={episodio.mal_id}
+                      className="mb-5 flex flex-col max-w-sm mx-auto w-full"
+                    >
+                      <p className="capitalize">episodio {episodio.mal_id}</p>
+                      <div className="rounded-xl overflow-hidden border-2 border-main-black my-1">
+                        <img src={episodio.images.jpg.image_url} alt="" />
+                      </div>
+                      <p className="self-end">{episodio.title}</p>
+                    </article>
+                  ))}
+                </div>
+              </SectionDetalle>
+            )}
+        </>
       )}
-    </SectionDetalle>
+    </>
   );
 };
 
