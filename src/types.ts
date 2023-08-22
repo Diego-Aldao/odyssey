b6e68dd75553f14b4dcfdfb1218eee9c3da71f72
Diego Aldao <diego_ind@live.com.ar>
2023-08-22 19:38:42 -0300

    añadidos mas types para las respuestas del fetch y agregado un type para cada tipo de response de la pagina detalle

--- src/types.ts
+++ src/types.ts
@@ -29,7 +29,14 @@ export type Filtro = {
 
 export type InfoAside = {
   nombre: string;
-  data: string | Date | number | never[] | ObjetoMal[] | undefined;
+  data:
+    | string
+    | Date
+    | number
+    | never[]
+    | ObjetoMal[]
+    | undefined
+    | Demographic;
 };
 
 export type InfoTags = {
@@ -42,81 +49,63 @@ export type InfoTags = {
   esRankeable?: boolean;
 };
 
-export type APIResult = {
-  data: Data;
-};
+/*TYPES FETCH*/
 
-export type SecondaryApiResult = {
-  pagination?: Pagination;
-  data:
-    | DataPersonaje[]
-    | DataEpisode[]
-    | DataRecomendados[]
-    | DataPersonaje[]
-    | DataReviews[];
+export type ApiResponseTemporada = {
+  pagination: PaginationTemporada;
+  data: DataTemporada[];
 };
-
-export type DataRecomendados = {
-  entry: Entry;
-  url: string;
-  votes: number;
+export type PaginationTemporada = {
+  last_visible_page: number;
+  has_next_page: boolean;
+  current_page: number;
+  items: Items;
 };
-
-export type DataPersonaje = {
-  character: Character;
-  role: Role;
-  favorites: number;
-  voice_actors: VoiceActor[];
-};
-
-export type DataEpisode = {
-  mal_id: number;
-  title: string;
-  episode: string;
-  url: string;
-  images: ImagesSecondary;
-};
-
-export type DataReviews = {
-  mal_id: number;
-  url: string;
-  type: Type;
-  reactions: Reactions;
-  date: Date;
-  review: string;
-  score: number;
-  tags: Tag[];
-  is_spoiler: boolean;
-  is_preliminary: boolean;
-  episodes_watched: null;
-  user: User;
-};
-
-export type DataNews = {
-  mal_id: number;
-  url: string;
-  title: string;
-  date: Date;
-  author_username: string;
-  author_url: string;
-  forum_url: string;
-  images: ImagesNews;
-  comments: number;
-  excerpt: string;
-};
-
-export type ImagesNews = {
-  jpg: Jpg;
-};
-
-export type Entry = {
+export type DataTemporada = {
   mal_id: number;
   url: string;
   images: { [key: string]: Image };
+  trailer: Trailer;
+  approved: boolean;
+  titles: Title[];
   title: string;
+  title_english: null | string;
+  title_japanese: string;
+  title_synonyms: string[];
+  type: DatumType;
+  source: Source;
+  episodes: number | null;
+  status: Status;
+  airing: boolean;
+  aired: Aired;
+  duration: string;
+  rating: Rating;
+  score: number;
+  scored_by: number;
+  rank: number;
+  popularity: number;
+  members: number;
+  favorites: number;
+  synopsis: string;
+  background: null | string;
+  season: Season | null;
+  year: number | null;
+  broadcast: Broadcast;
+  producers: Demographic[];
+  licensors: Demographic[];
+  studios: Demographic[];
+  genres: Demographic[];
+  explicit_genres: [];
+  themes: Demographic[];
+  demographics: Demographic[];
 };
 
-export type Data = {
+/*types fetch pagina detalle */
+
+export type ApiResponseDetalle = {
+  data: DataDetalle;
+};
+export type DataDetalle = {
   mal_id: number;
   url: string;
   images: { [key: string]: Image };
@@ -142,21 +131,101 @@ export type Data = {
   members: number;
   favorites: number;
   synopsis: string;
-  background: string;
+  background: null;
   season: string;
   year: number;
   broadcast: Broadcast;
-  producers: Demographic[];
+  producers: Generic[];
   licensors: [];
-  studios: Demographic[];
-  genres: Demographic[];
+  studios: Generic[];
+  genres: Generic[];
   explicit_genres: [];
-  themes: Demographic[];
-  demographics: Demographic[];
+  themes: Generic[];
+  demographics: Demographic;
   relations: Relation[];
   theme: Theme;
   external: External[];
   streaming: External[];
+};
+
+export type ApiResponseDetalleEpisodios = {
+  pagination: Pagination;
+  data: DataEpisode[];
+};
+export type DataEpisode = {
+  mal_id: number;
+  title: string;
+  episode: string;
+  url: string;
+  images: ImagesSecondary;
+};
+
+export type ApiResponseDetalleNoticias = {
+  pagination: Pagination;
+  data: DataNews[];
+};
+export type DataNews = {
+  mal_id: number;
+  url: string;
+  title: string;
+  date: Date;
+  author_username: string;
+  author_url: string;
+  forum_url: string;
+  images: ImagesNews;
+  comments: number;
+  excerpt: string;
+};
+
+export type ApiResponseDetallePersonajes = {
+  pagination: Pagination;
+  data: DataPersonaje[];
+};
+export type DataPersonaje = {
+  character: Character;
+  role: Role;
+  favorites: number;
+  voice_actors: VoiceActor[];
+};
+
+export type ApiResponseDetalleRecomendado = {
+  pagination: Pagination;
+  data: DataRecomendados[];
+};
+export type DataRecomendados = {
+  entry: Entry;
+  url: string;
+  votes: number;
+};
+
+export type ApiResponseDetalleReviews = {
+  pagination: Pagination;
+  data: DataReviews[];
+};
+export type DataReviews = {
+  mal_id: number;
+  url: string;
+  type: Type;
+  reactions: Reactions;
+  date: Date;
+  review: string;
+  score: number;
+  tags: Tag[];
+  is_spoiler: boolean;
+  is_preliminary: boolean;
+  episodes_watched: null;
+  user: User;
+};
+
+export type ImagesNews = {
+  jpg: Jpg;
+};
+
+export type Entry = {
+  mal_id: number;
+  url: string;
+  images: { [key: string]: Image };
+  title: string;
 };
 
 export type Aired = {
@@ -320,4 +389,67 @@ export type User = {
   url: string;
   username: string;
   images: { [key: string]: Image };
+};
+
+export enum Timezone {
+  AsiaTokyo = "Asia/Tokyo",
+}
+
+export enum DemographicType {
+  Anime = "anime",
+}
+
+export enum Rating {
+  PG13Teens13OrOlder = "PG-13 - Teens 13 or older",
+  R17ViolenceProfanity = "R - 17+ (violence & profanity)",
+}
+
+export enum Season {
+  Summer = "summer",
+}
+
+export enum Source {
+  LightNovel = "Light novel",
+  Manga = "Manga",
+  Novel = "Novel",
+  Original = "Original",
+}
+
+export enum Status {
+  CurrentlyAiring = "Currently Airing",
+  FinishedAiring = "Finished Airing",
+}
+
+export enum TitleType {
+  Default = "Default",
+  English = "English",
+  Japanese = "Japanese",
+  Synonym = "Synonym",
+}
+
+export type ImagesTemporada = {
+  image_url: null | string;
+  small_image_url: null | string;
+  medium_image_url: null | string;
+  large_image_url: null | string;
+  maximum_image_url: null | string;
+};
+
+export enum DatumType {
+  Ona = "ONA",
+  Special = "Special",
+  Tv = "TV",
+}
+
+export type Items = {
+  count: number;
+  total: number;
+  per_page: number;
+};
+
+export type Generic = {
+  mal_id: number;
+  type: Type;
+  name: string;
+  url: string;
 };
