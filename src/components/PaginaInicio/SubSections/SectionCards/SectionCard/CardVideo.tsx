a848f77dfc734a97a6ff37bf8061f85bd064d73f
Diego Aldao <diego_ind@live.com.ar>
2023-08-24 19:15:09 -0300

    ahora el componente no recibe por props ni id ni textBlack, corregidas las clases

--- src/components/PaginaInicio/SubSections/SectionCards/SectionCard/CardVideo.tsx
+++ src/components/PaginaInicio/SubSections/SectionCards/SectionCard/CardVideo.tsx
@@ -2,51 +2,36 @@ import { Icon } from "@iconify/react";
 import { Link } from "react-router-dom";
 
 type Props = {
-  imagen: string;
+  imagen?: string;
   titulo: string;
   subtitulo: string | undefined;
-  textBlack?: boolean;
+
   id: number;
 };
 
-const CardVideo: React.FC<Props> = ({
-  imagen,
-  titulo,
-  subtitulo,
-  textBlack,
-  id,
-}) => {
+const CardVideo: React.FC<Props> = ({ imagen, titulo, subtitulo, id }) => {
   return (
     <div
-      className={`border-2 relative section_card_video ${
-        textBlack
-          ? "border-main-black section_card_video_text_black"
-          : "border-main-color-background"
-      } rounded-tr-[60px] rounded-bl-[60px] rounded-xl my-8`}
+      className="border-2 relative section_card_video border-main-color-background
+       rounded-tr-[60px] rounded-bl-[60px] rounded-xl my-8"
     >
-      <div
-        className={`absolute -top-7 left-0 rounded-tl-lg  rounded-br-lg ${
-          textBlack ? "text-main-black" : "text-main-color-background"
-        }`}
-      >
-        <p className="text-[12px] xl:text-sm">{subtitulo}</p>
+      <div className="absolute -top-7 left-0 rounded-tl-lg  rounded-br-lg text-main-color-background">
+        <p className="text-[12px] xl:text-sm">{titulo}</p>
       </div>
-      <div className="btn-play bg-main-black text-main-color-background h-12 w-12 rounded-full flex items-center justify-center absolute">
-        <Icon
-          icon="streamline:entertainment-control-button-play-button-television-buttons-movies-play-tv-video-controls"
-          className="w-5 h-5"
-        />
-      </div>
-      <div className="overflow-hidden rounded-tr-[60px] rounded-bl-[60px] rounded-xl">
+
+      <div className="overflow-hidden rounded-tr-[60px] rounded-bl-[60px] rounded-xl flex items-center justify-center">
+        <div className="absolute bg-main-black text-main-color-background rounded-lg px-5 capitalize flex items-center justify-center gap-4 py-1 border-[1px] border-main-color-background">
+          ver video
+          <Icon
+            icon="streamline:entertainment-control-button-play-button-television-buttons-movies-play-tv-video-controls"
+            className="stroke-2"
+          />
+        </div>
         <img src={imagen} alt="" />
       </div>
-      <div
-        className={`absolute -bottom-7 -right-[2px] rounded-b-lg ${
-          textBlack ? "text-main-black" : "text-main-color-background"
-        }`}
-      >
+      <div className=" absolute -bottom-7 -right-[2px] rounded-b-lg text-main-color-background">
         <Link to={`/detalle/anime/${id}`}>
-          <p className="text-[12px] xl:text-sm">{titulo}</p>
+          <p className="text-[12px] xl:text-sm">{subtitulo}</p>
         </Link>
       </div>
     </div>
