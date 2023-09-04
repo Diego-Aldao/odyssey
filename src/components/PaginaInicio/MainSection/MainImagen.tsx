0f5623862cfa834e7efe8684f66c5b81b0948cff
Diego Aldao <diego_ind@live.com.ar>
2023-09-04 20:00:39 -0300

    añadido el componente que renderiza la imagen principal de lse seccion main

---
+++ src/components/PaginaInicio/MainSection/MainImagen.tsx
@@ -0,0 +1,11 @@
+import mainAnime from "../../../assets/animeMain2.png";
+
+const MainImagen = () => {
+  return (
+    <div className="hidden xs:block xs:col-start-2 xs:col-span-2 personaje h-[100vw] max-h-[450px] md:max-h-[550px] lg:max-h-[700px] md:h-[60vw] md:absolute -top-0 -right-8">
+      <img src={mainAnime} alt="" className="object-contain" />
+    </div>
+  );
+};
+
+export default MainImagen;
