3882698708d4277a0b22c157ef74864f9cbe8d37
Diego Aldao <diego_ind@live.com.ar>
2023-09-04 19:59:02 -0300

    removidas las cards anime y personaje de la seccion main, agregado el componente lazy load de react lazy, agregado el componente con la imagen principal de la seccion main y agregada la prop destino para los componentes section header

--- src/pages/PaginaInicio.tsx
+++ src/pages/PaginaInicio.tsx
@@ -1,9 +1,5 @@
 import MainLayout from "../layout/MainLayout";
-import personaje from "../assets/personaje.jpg";
-import animeWallpaper from "../assets/anime.jpg";
 import MainTitulo from "../components/PaginaInicio/MainSection/MainTitulo";
-import SectionCardPersonaje from "../components/PaginaInicio/MainSection/MainCardPersonaje";
-import SectionCardAnime from "../components/PaginaInicio/MainSection/MainCardAnime";
 import MainButtonRandom from "../components/PaginaInicio/MainSection/MainButtonRandom";
 import MainInfoApp from "../components/PaginaInicio/MainSection/MainInfoApp";
 import SectionHeader from "../components/PaginaInicio/SubSections/SectionHeader";
@@ -14,50 +10,73 @@ import SectionTops from "../components/PaginaInicio/SubSections/Tops/SectionTops
 import AnimesTopHistoria from "../components/PaginaInicio/SubSections/Tops/AnimesTopHistoria";
 import SectionEpisodiosRecientes from "../components/PaginaInicio/SubSections/Episodios/SectionEpisodiosRecientes";
 import SectionTrailersPopulares from "../components/PaginaInicio/SubSections/Trailers/SectionTrailersPopulares";
+import LazyLoad from "react-lazy-load";
+import MainImagen from "../components/PaginaInicio/MainSection/MainImagen";
 
 const PaginaInicio = () => {
   return (
     <MainLayout>
       <>
-        <section className="main_section">
+        <section className="main_section grid gap-10 xs:gap-0 bg-main-color-background w-full rounded-b-2xl px-4 md:px-6 lg:px-10 py-8 lg:py-10 lg:pt-20 xs:grid xs:grid-cols-3 relative md:grid-rows-[1fr,100px,200px] lg:grid-rows-[1fr,100px,200px] xl:grid-rows-[1fr,150px,200px]">
           <MainTitulo texto={"odyssey: comunidad anime"} />
-          <SectionCardPersonaje personaje={personaje} />
+          <MainImagen />
           <MainButtonRandom />
-          <SectionCardAnime animeWallpaper={animeWallpaper} />
           <MainInfoApp />
         </section>
-        <SectionEpisodiosRecientes>
-          <SectionHeader
-            titulo="episodios recientes"
-            nombreIcono="streamline:interface-time-alarm-notification-alert-bell-wake-clock-alarm"
-            nombreInteraccion="ver mas episodios"
-          />
-        </SectionEpisodiosRecientes>
-        <SectionAnimeTemporada>
-          <SectionHeader
-            titulo="anime de temporada"
-            nombreIcono="streamline:interface-calendar-favorite-calendar-date-day-favorite-like-month-star"
-            nombreInteraccion="ver más temporadas"
-          />
-        </SectionAnimeTemporada>
-        <SectionTrailersPopulares noBackground={true}>
-          <SectionHeader
-            titulo="trailers populares"
-            nombreIcono="streamline:computer-logo-youtube-youtube-clip-social-video"
-            nombreInteraccion="ver mas trailers"
-            noBackground={true}
-          />
-        </SectionTrailersPopulares>
+        <LazyLoad
+          offset={500}
+          className="min-h-[480px] bg-main-color-background rounded-xl"
+        >
+          <SectionEpisodiosRecientes>
+            <SectionHeader
+              destino="episodios"
+              titulo="episodios recientes"
+              nombreIcono="streamline:interface-time-alarm-notification-alert-bell-wake-clock-alarm"
+              nombreInteraccion="ver mas episodios"
+            />
+          </SectionEpisodiosRecientes>
+        </LazyLoad>
+        <LazyLoad
+          offset={500}
+          className="min-h-[480px] bg-main-color-background rounded-xl"
+        >
+          <SectionAnimeTemporada>
+            <SectionHeader
+              destino="temporadas"
+              titulo="anime de temporada"
+              nombreIcono="streamline:interface-calendar-favorite-calendar-date-day-favorite-like-month-star"
+              nombreInteraccion="ver más temporadas"
+            />
+          </SectionAnimeTemporada>
+        </LazyLoad>
+        <LazyLoad offset={500} className="min-h-[363px]">
+          <SectionTrailersPopulares noBackground={true}>
+            <SectionHeader
+              destino="trailers"
+              titulo="trailers populares"
+              nombreIcono="streamline:computer-logo-youtube-youtube-clip-social-video"
+              nombreInteraccion="ver mas trailers"
+              noBackground={true}
+            />
+          </SectionTrailersPopulares>
+        </LazyLoad>
         <SectionTops>
           <>
             <SectionHeader
+              destino="tops"
               titulo="top anime"
               nombreIcono="streamline:interface-award-crown-reward-social-rating-media-queen-vip-king-crown"
               nombreInteraccion="ver todos los tops"
             />
-            <AnimesTop />
-            <AnimesTopHistoria />
-            <PersonajesTop />
+            <LazyLoad offset={800} className="min-h-[424px]">
+              <AnimesTop />
+            </LazyLoad>
+            <LazyLoad offset={500} className="min-h-[424px]">
+              <AnimesTopHistoria />
+            </LazyLoad>
+            <LazyLoad offset={500} className="min-h-[424px]">
+              <PersonajesTop />
+            </LazyLoad>
           </>
         </SectionTops>
       </>
