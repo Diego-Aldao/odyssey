69a341dd9eefc53c09e6fea863a537a0cf65f2d6
Diego Aldao <diego_ind@live.com.ar>
2023-09-04 20:00:07 -0300

    removidos imports inutilizados y reemplazado el componente imagenesPersonaje por SectionPictures

--- src/components/PaginaDetalle/DetallePersonaje/DetallePersonaje.tsx
+++ src/components/PaginaDetalle/DetallePersonaje/DetallePersonaje.tsx
@@ -1,17 +1,13 @@
-import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import useFetch from "../../../hooks/useFetch";
 import { BASE_URL_DETAILS_CHARACTER } from "../../../constants";
-import fakeRespuesta from "../../../mocks/DetallePersonajes.json";
 import ImagenBackground from "../ImagenBackground";
-import FiltrosDetalle from "../FiltrosDetalle";
-import SectionDetalle from "../SectionDetalle";
 import PersonajeInfo from "./PersonajeInfo";
 import AsidePersonaje from "./AsidePersonaje";
 import HeaderPersonaje from "./HeaderPersonaje";
 import DoblajePersonaje from "./DoblajePersonaje";
-import ImagenesPersonaje from "./ImagenesPersonaje";
 import { ApiResponsePersonaje } from "../../../types";
+import SectionPictures from "../SectionPictures";
 
 const DetallePersonaje = () => {
   const { id } = useParams();
@@ -41,7 +37,11 @@ const DetallePersonaje = () => {
             />
             <PersonajeInfo info={respuestaApi.data.about} />
             <DoblajePersonaje dataDoblaje={respuestaApi.data.voices} />
-            <ImagenesPersonaje id={id} />
+            <SectionPictures
+              id={id}
+              nombre={respuestaApi.data.name}
+              baseUrl={BASE_URL_DETAILS_CHARACTER}
+            />
           </section>
         </>
       )}
