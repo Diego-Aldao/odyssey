a31e1c4c24a2f9376c215b2ce94a74d05b7adcf3
Diego Aldao <diego_ind@live.com.ar>
2023-09-04 20:01:43 -0300

    corregido el z-index para no colapsar con el menu mobile

--- src/components/PaginaInicio/SubSections/SectionCards/CardInfo/Footer.tsx
+++ src/components/PaginaInicio/SubSections/SectionCards/CardInfo/Footer.tsx
@@ -12,7 +12,7 @@ const Footer: React.FC<Props> = ({ score, miembros }) => {
     <footer className="w-full flex items-center justify-center border-main-black border-b-2  xs:border-b-4 md:border-b-[6px]">
       <ul
         className="flex text-[10px] text-main-color-background gap-2 px-2 xs:gap-3
-    md:justify-evenly md:text-xs bg-main-black item-skew-x z-10 relative after:-right-[10px] after:w-[50px] after:skew-x-[-331deg] after:rounded-b-none after:rounded-t-[5px] before:rounded-t-[5px] before:-left-[10px] before:w-[50px] before:skew-x-[331deg] before:rounded-b-none  xs:py-1"
+    md:justify-evenly md:text-xs bg-main-black item-skew-x z-[1] relative after:-right-[10px] after:w-[50px] after:skew-x-[-331deg] after:rounded-b-none after:rounded-t-[5px] before:rounded-t-[5px] before:-left-[10px] before:w-[50px] before:skew-x-[331deg] before:rounded-b-none  xs:py-1"
       >
         <li className="flex gap-1 items-center">
           <Icon icon="streamline:interface-favorite-star-reward-rating-rate-social-star-media-favorite-like-stars" />
