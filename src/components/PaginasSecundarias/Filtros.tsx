465995036e2e154b31d803df35e1a99b37a0f669
Diego Aldao <diego_ind@live.com.ar>
2023-09-05 18:56:32 -0300

    creado un componente reutilizable para los filtros de las paginas principales

---
+++ src/components/PaginasSecundarias/Filtros.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Filtro } from "../../types";
+import { Link, useLocation } from "react-router-dom";
+
+type Props = {
+  filtros: Filtro[];
+  pathInicial: string;
+  isFilterSecondary?: boolean;
+  setCurrentFiltro?: React.Dispatch<React.SetStateAction<string | undefined>>;
+};
+
+const Filtros: React.FC<Props> = ({
+  filtros,
+  pathInicial,
+  isFilterSecondary,
+  setCurrentFiltro,
+}) => {
+  const location = useLocation();
+  const hideFilter =
+    location.pathname.includes("personajes") ||
+    location.pathname.includes("personas");
+
+  return (
+    <>
+      {hideFilter && isFilterSecondary ? (
+        <></>
+      ) : (
+        <ul className="item-skew-izquierda z-[1] relative px-5 md:px-7 lg:px-11 -left-5 md:-left-6 lg:-left-10 py-3 bg-main-black flex items-center gap-2 xs:gap-4 capitalize max-w-fit my-5 lg:mt-10">
+          {filtros.map((filtro) => (
+            <>
+              {isFilterSecondary && setCurrentFiltro ? (
+                <li
+                  onClick={() => {
+                    setCurrentFiltro(filtro.query);
+                  }}
+                  key={filtro.id}
+                  className="capitalize text-main-color-background text-[10px] xs:text-xs md:text-sm lg:text-base"
+                >
+                  {filtro.nombre}
+                </li>
+              ) : (
+                <li
+                  key={filtro.id}
+                  className="capitalize text-main-color-background text-[10px] xs:text-xs md:text-sm lg:text-base"
+                >
+                  <Link to={`/${pathInicial}/${filtro.query || ""}`}>
+                    {filtro.nombre}
+                  </Link>
+                </li>
+              )}
+            </>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
+
+export default Filtros;
