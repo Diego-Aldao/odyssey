7dbe70ba9098a6260a636782385ec38a252006e4
Diego Aldao <diego_ind@live.com.ar>
2023-09-05 18:53:18 -0300

    corregidas las clases tailwind para que el alto del titulo sea automatico

--- src/components/PaginaInicio/SubSections/SectionCards/CardInfo/CardInfo.tsx
+++ src/components/PaginaInicio/SubSections/SectionCards/CardInfo/CardInfo.tsx
@@ -12,7 +12,7 @@ const CardInfo: React.FC<Props> = ({ titulo, id, tipo, children }) => {
   return (
     <Link to={`/detalle/${currentTipo}/${id}`} className="w-full relative flex">
       <article className="relative flex flex-col gap-2 xs:gap-3 md:rounded-none  bg-main-color-background w-full">
-        <h3 className="text-center h-[32px] xs:h-[50px] md:h-[75px] xl:h-[90px] flex items-end justify-center w-full font-bold text-xs xs:text-sm md:text-base xl:text-lg leading-1 line-clamp-2">
+        <h3 className="text-center flex-auto h-full w-full font-bold text-xs xs:text-sm md:text-base xl:text-lg leading-1 line-clamp-2">
           {titulo}
         </h3>
         {children}
