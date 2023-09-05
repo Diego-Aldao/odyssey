033638b65283332ea0d0110cecad861a38a23e98
Diego Aldao <diego_ind@live.com.ar>
2023-09-05 18:47:54 -0300

    cambiada la imagen y reemplazadas todas las clases tailwind por una clase custom

--- src/components/PaginaInicio/MainSection/MainImagen.tsx
+++ src/components/PaginaInicio/MainSection/MainImagen.tsx
@@ -1,8 +1,8 @@
-import mainAnime from "../../../assets/ImagenPrincipal.png";
+import mainAnime from "../../../assets/imgPrincipal.png";
 
 const MainImagen = () => {
   return (
-    <div className="hidden xs:block xs:col-start-2 xs:col-span-2 personaje h-[100vw] max-h-[450px] md:max-h-[550px] lg:max-h-[700px] md:h-[60vw] md:absolute -top-0 -right-8">
+    <div className="main_imagen">
       <img src={mainAnime} alt="" className="object-contain" />
     </div>
   );
