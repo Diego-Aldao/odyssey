f33749d638120a4f5edb3c5435f22c51c4dcc881
Diego Aldao <diego_ind@live.com.ar>
2023-09-11 17:40:05 -0300

    cambiado el icono y sus tamaños

--- src/components/Layout/BotonLogin.tsx
+++ src/components/Layout/BotonLogin.tsx
@@ -2,11 +2,11 @@ import { Icon } from "@iconify/react";
 
 const BotonLogin = () => {
   return (
-    <div className="hidden md:flex items-center justify-center lg:justify-end xl:justify-center gap-2 h-full w-1/12 lg:w-fit lg:pl-3">
+    <div className="hidden md:flex items-center justify-center lg:justify-end xl:justify-center gap-1 h-full w-1/12 lg:w-fit lg:pl-3">
       <p className="capitalize font-bold hidden lg:block text-lg">ingresar</p>
       <Icon
-        icon="fluent-emoji-high-contrast:bust-in-silhouette"
-        className="h-6 w-6"
+        icon="game-icons:ninja-heroic-stance"
+        className="lg:h-6 h-10 lg:w-6 w-10"
       />
     </div>
   );
