ec2be34b73fba2008ef337e81707269205ac36c7
Diego Aldao <diego_ind@live.com.ar>
2023-09-11 17:41:19 -0300

    añadido un componente reutilizable para las secciones de la pagina busqueda

---
+++ src/components/PaginaBusqueda/SectionBusqueda.tsx
@@ -0,0 +1,23 @@
+type Props = {
+  seccion: string;
+  children: JSX.Element;
+  currentContent: string | undefined;
+};
+
+const SectionBusqueda = ({ seccion, children, currentContent }: Props) => {
+  const seccionVisible =
+    seccion === currentContent ||
+    currentContent === "general" ||
+    !currentContent;
+
+  return (
+    <section className={`py-10 ${seccionVisible ? "block" : "hidden"}`}>
+      <p className="capitalize item-skew-izquierda z-[1] relative bg-main-black text-main-color-background w-fit -left-4 pl-4 md:-left-6 lg:-left-10 md:pl-6 lg:pl-10 md:px-10 py-1 xs:text-lg md:text-xl">
+        resultados en {seccion}
+      </p>
+      {children}
+    </section>
+  );
+};
+
+export default SectionBusqueda;
