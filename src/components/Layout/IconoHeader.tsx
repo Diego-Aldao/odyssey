49ac2f29e91043ade25dcdd13005badeaa30708c
Diego Aldao <diego_ind@live.com.ar>
2023-09-20 12:00:43 -0300

    ahora el componente puede recibir clases por props, añadido motion para la animacion del icon

--- src/components/Layout/IconoHeader.tsx
+++ src/components/Layout/IconoHeader.tsx
@@ -1,16 +1,36 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import { transition, variantesIconoHeader } from "../../VariantesFramerMotion";
 
-const IconoHeader = () => {
+type Props = {
+  clasesTexto?: string;
+  clasesIcono?: string;
+};
+
+const IconoHeader: React.FC<Props> = ({ clasesTexto, clasesIcono }) => {
   return (
-    <div className="h-full flex items-center w-3/4 icono md:w-1/6 lg:w-1/5">
-      <span className="font-semibold text-2xl md:text-base lg:text-2xl xl:text-3xl">
-        odyssey
-      </span>
-      <Icon
-        icon="game-icons:mine-explosion"
-        className="ml-1 w-6 h-6 lg:w-8 lg:h-8 lg:ml-2"
-      />
-    </div>
+    <Link to="/">
+      <div
+        className={`h-full flex items-center ${
+          clasesTexto
+            ? clasesTexto
+            : "w-3/4 gap-1 lg:gap-2 md:w-2/12 lg:w-fit lg:pr-3 text-2xl md:text-base lg:text-2xl xl:text-3xl"
+        }`}
+      >
+        <span className="font-semibold">Odyssey</span>
+        <motion.span
+          animate="animate"
+          variants={variantesIconoHeader}
+          transition={{ duration: 0.1, repeat: 2, transition }}
+        >
+          <Icon
+            icon="game-icons:mine-explosion"
+            className={clasesIcono ? clasesIcono : " w-6 h-6 lg:w-8 lg:h-8"}
+          />
+        </motion.span>
+      </div>
+    </Link>
   );
 };
 
