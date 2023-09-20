c8b45032fe968f7a48a782ea8e3c07113b58c109
Diego Aldao <diego_ind@live.com.ar>
2023-09-20 12:21:12 -0300

    añadido el componente loading para mostrar mienstras se ejecuta el fetch y el componente motionUL

--- src/components/PaginaDetalle/SectionPictures.tsx
+++ src/components/PaginaDetalle/SectionPictures.tsx
@@ -1,6 +1,8 @@
 import useFetch from "../../hooks/useFetch";
 import { ApiResponseDetallePictures } from "../../types";
+import Loading from "../Generales/Loading";
 import MotionListItem from "./FramerMotion/MotionListItem";
+import MotionUnorderedList from "./FramerMotion/MotionUnorderedList";
 import SubSectionDetalle from "./SubSectionDetalle";
 
 type Props = {
@@ -17,16 +19,21 @@ const SectionPictures = ({ id, nombre, baseUrl }: Props) => {
   return (
     <>
       {loading || !respuestaApi ? (
-        <>cargando</>
+        <Loading />
       ) : (
-        <SubSectionDetalle titulo={`imagenes de ${nombre}`}>
-          <ul className="grid grid-cols-[repeat(auto-fill,minmax(130px,1fr))] lg:grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-2 ">
-            {respuestaApi.data.map((imagen) => (
-              <MotionListItem clases="rounded-lg border-2 border-main-black overflow-hidden">
-                <img src={imagen.jpg.image_url} alt="" />
-              </MotionListItem>
-            ))}
-          </ul>
+        <SubSectionDetalle
+          titulo={`imagenes de ${nombre}`}
+          visibleContent="general"
+        >
+          <MotionUnorderedList clases="grid grid-cols-[repeat(auto-fill,minmax(130px,1fr))] lg:grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-2 ">
+            <>
+              {respuestaApi.data.map((imagen) => (
+                <MotionListItem clases="rounded-lg border-2 border-main-black overflow-hidden">
+                  <img src={imagen.jpg.image_url} alt="" />
+                </MotionListItem>
+              ))}
+            </>
+          </MotionUnorderedList>
         </SubSectionDetalle>
       )}
     </>
