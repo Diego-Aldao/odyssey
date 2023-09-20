411abb9930bbc2c00560e569f41e26475a88efe4
Diego Aldao <diego_ind@live.com.ar>
2023-09-20 12:08:35 -0300

    añadido el componente loading, animatePrecense y motion para las animaciones, y listadoSinResultados para cuando el fetch devuelve un array vacio. agregado un state y effect para saber el largo de una query

--- src/components/PaginaBusqueda/ResultadosPersonajes.tsx
+++ src/components/PaginaBusqueda/ResultadosPersonajes.tsx
@@ -1,47 +1,71 @@
+import { VarianteSections, transition } from "../../VariantesFramerMotion";
 import { BASE_URL_CHARACTER } from "../../constants";
 import useFetch from "../../hooks/useFetch";
 import { ApiResponseTopPersonaje } from "../../types";
+import Loading from "../Generales/Loading";
+import ListadoSinResultados from "../PaginaDetalle/DetalleAnime/MainSection/ListadoSinResultados";
 import CardBusqueda from "./CardBusqueda";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState, useEffect } from "react";
 
 type Props = {
   query: string | undefined;
 };
 
 const ResultadosPersonaje = ({ query }: Props) => {
+  const [queryValida, setQueryValida] = useState<boolean>();
   const url = `${BASE_URL_CHARACTER}?q=${query || ""}`;
   const { respuestaApi, loading } = useFetch<ApiResponseTopPersonaje>(url);
 
+  useEffect(() => {
+    if (query) {
+      setQueryValida(query.length >= 4 ? true : false);
+    }
+  }, [query]);
+
   return (
-    <div className="grid gap-2 mt-10 md:grid-cols-2 md:gap-4 lg:grid-cols-3">
-      <>
-        {loading || !respuestaApi ? (
-          <>cargando...</>
-        ) : (
-          <>
-            {respuestaApi.data.map((personaje) => (
-              <CardBusqueda
-                titulo={personaje.name}
-                imagen={personaje.images.webp.image_url}
-                id={personaje.mal_id}
-                tipo="personaje"
-              >
-                <>
-                  {personaje.name_kanji && (
-                    <p className="text-xs font-semibold">
-                      ({personaje.name_kanji})
+    <AnimatePresence>
+      {loading || !respuestaApi ? (
+        <Loading key={"loading"} />
+      ) : (
+        <>
+          {queryValida && respuestaApi.data?.length >= 1 ? (
+            <motion.div
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              variants={VarianteSections}
+              transition={transition}
+              key={"resultados"}
+              className="grid gap-2 md:grid-cols-2 md:gap-4 lg:grid-cols-3"
+            >
+              {respuestaApi.data.map((personaje) => (
+                <CardBusqueda
+                  titulo={personaje.name}
+                  imagen={personaje.images.webp.image_url}
+                  id={personaje.mal_id}
+                  tipo="personaje"
+                >
+                  <>
+                    {personaje.name_kanji && (
+                      <p className="text-xs font-semibold">
+                        ({personaje.name_kanji})
+                      </p>
+                    )}
+                    <p className="text-xs capitalize font-semibold">
+                      favoritos:{" "}
+                      <span className="font-normal">{personaje.favorites}</span>
                     </p>
-                  )}
-                  <p className="text-xs capitalize font-semibold">
-                    favoritos:{" "}
-                    <span className="font-normal">{personaje.favorites}</span>
-                  </p>
-                </>
-              </CardBusqueda>
-            ))}
-          </>
-        )}
-      </>
-    </div>
+                  </>
+                </CardBusqueda>
+              ))}
+            </motion.div>
+          ) : (
+            <ListadoSinResultados nombreSeccion="personajes" />
+          )}
+        </>
+      )}
+    </AnimatePresence>
   );
 };
 
--- src/components/PaginaBusqueda/ResultadosPersonas.tsx
+++ src/components/PaginaBusqueda/ResultadosPersonas.tsx
@@ -1,68 +1,91 @@
+import { AnimatePresence, motion } from "framer-motion";
 import { BASE_URL_PEOPLE } from "../../constants";
 import useFetch from "../../hooks/useFetch";
 import { ApiResponseTopPersona } from "../../types";
+import Loading from "../Generales/Loading";
 import CardBusqueda from "./CardBusqueda";
+import { VarianteSections, transition } from "../../VariantesFramerMotion";
+import ListadoSinResultados from "../PaginaDetalle/DetalleAnime/MainSection/ListadoSinResultados";
+import { useState, useEffect } from "react";
 
 type Props = {
   query: string | undefined;
 };
 
 const ResultadosPersonas = ({ query }: Props) => {
+  const [queryValida, setQueryValida] = useState<boolean>();
   const url = `${BASE_URL_PEOPLE}?q=${query || ""}`;
   const { respuestaApi, loading } = useFetch<ApiResponseTopPersona>(url);
 
+  useEffect(() => {
+    if (query) {
+      setQueryValida(query.length >= 4 ? true : false);
+    }
+  }, [query]);
+
   return (
-    <div className="grid gap-2 mt-10 md:grid-cols-2 md:gap-4 lg:grid-cols-3">
-      <>
-        {loading || !respuestaApi ? (
-          <>cargando...</>
-        ) : (
-          <>
-            {respuestaApi.data.map((persona) => {
-              let fechaCumpleaños;
-              if (persona.birthday) {
-                fechaCumpleaños = new Date(persona.birthday).toLocaleDateString(
-                  "es-AR",
-                  {
+    <AnimatePresence>
+      {loading || !respuestaApi ? (
+        <Loading key={"loading"} />
+      ) : (
+        <>
+          {queryValida && respuestaApi.data?.length >= 1 ? (
+            <motion.div
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              variants={VarianteSections}
+              transition={transition}
+              key={"resultados"}
+              className="grid gap-2 md:grid-cols-2 md:gap-4 lg:grid-cols-3"
+            >
+              {respuestaApi.data.map((persona) => {
+                let fechaCumpleaños;
+                if (persona.birthday) {
+                  fechaCumpleaños = new Date(
+                    persona.birthday
+                  ).toLocaleDateString("es-AR", {
                     day: "numeric",
                     month: "long",
                     year: "numeric",
-                  }
+                  });
+                }
+
+                return (
+                  <CardBusqueda
+                    titulo={persona.name}
+                    imagen={persona.images.jpg.image_url}
+                    id={persona.mal_id}
+                    tipo="persona"
+                  >
+                    <>
+                      {persona.given_name && (
+                        <p className="text-xs font-semibold">
+                          ({persona.given_name} {persona.family_name})
+                        </p>
+                      )}
+                      {fechaCumpleaños && (
+                        <p className="text-xs font-semibold">
+                          cumpleaños:{" "}
+                          <span className="font-normal">{fechaCumpleaños}</span>
+                        </p>
+                      )}
+
+                      <p className="text-xs capitalize font-semibold">
+                        favoritos:{" "}
+                        <span className="font-normal">{persona.favorites}</span>
+                      </p>
+                    </>
+                  </CardBusqueda>
                 );
-              }
-
-              return (
-                <CardBusqueda
-                  titulo={persona.name}
-                  imagen={persona.images.jpg.image_url}
-                  id={persona.mal_id}
-                  tipo="persona"
-                >
-                  <>
-                    {persona.given_name && (
-                      <p className="text-xs font-semibold">
-                        ({persona.given_name} {persona.family_name})
-                      </p>
-                    )}
-                    {fechaCumpleaños && (
-                      <p className="text-xs font-semibold">
-                        cumpleaños:{" "}
-                        <span className="font-normal">{fechaCumpleaños}</span>
-                      </p>
-                    )}
-
-                    <p className="text-xs capitalize font-semibold">
-                      favoritos:{" "}
-                      <span className="font-normal">{persona.favorites}</span>
-                    </p>
-                  </>
-                </CardBusqueda>
-              );
-            })}
-          </>
-        )}
-      </>
-    </div>
+              })}
+            </motion.div>
+          ) : (
+            <ListadoSinResultados nombreSeccion="personas" />
+          )}
+        </>
+      )}
+    </AnimatePresence>
   );
 };
 
