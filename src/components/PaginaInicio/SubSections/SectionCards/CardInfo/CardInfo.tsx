00120cc113326eb094e2a55a9ca277b26821907b
Diego Aldao <diego_ind@live.com.ar>
2023-09-21 12:04:40 -0300

    agregado el componente motion para las animaciones

--- src/components/PaginaInicio/SubSections/SectionCards/CardInfo/CardInfo.tsx
+++ src/components/PaginaInicio/SubSections/SectionCards/CardInfo/CardInfo.tsx
@@ -1,4 +1,9 @@
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import {
+  transition,
+  varianteCards,
+} from "../../../../../VariantesFramerMotion";
 
 type Props = {
   titulo: string;
@@ -10,14 +15,26 @@ type Props = {
 const CardInfo: React.FC<Props> = ({ titulo, id, tipo, children }) => {
   const currentTipo = tipo === "TV" ? "anime" : tipo?.toLowerCase();
   return (
-    <Link to={`/detalle/${currentTipo}/${id}`} className="w-full relative flex">
-      <article className="relative flex flex-col gap-2 xs:gap-3 md:rounded-none  bg-main-color-background w-full">
-        <h3 className=" flex-auto h-full w-full font-bold text-xs xs:text-sm md:text-base xl:text-lg flex items-center justify-center">
-          <span className="line-clamp-2 text-center">{titulo}</span>
-        </h3>
-        {children}
-      </article>
-    </Link>
+    <motion.div
+      initial="initial"
+      whileInView="whileInView"
+      viewport={{ once: true, margin: "-100px" }}
+      transition={transition}
+      variants={varianteCards}
+      className="w-full relative flex"
+    >
+      <Link
+        to={`/detalle/${currentTipo}/${id}`}
+        className="w-full relative flex"
+      >
+        <article className="relative flex flex-col gap-2 xs:gap-3 md:rounded-none  bg-main-color-background w-full">
+          <h3 className=" flex-auto h-full w-full font-bold text-xs xs:text-sm md:text-base xl:text-lg flex items-center justify-center">
+            <span className="line-clamp-2 text-center">{titulo}</span>
+          </h3>
+          {children}
+        </article>
+      </Link>
+    </motion.div>
   );
 };
 
