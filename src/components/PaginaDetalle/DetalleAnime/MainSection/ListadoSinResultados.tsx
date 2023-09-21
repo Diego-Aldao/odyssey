1715da90834300d75b1705cfae31754fd63c3e41
Diego Aldao <diego_ind@live.com.ar>
2023-09-21 12:08:23 -0300

    creado un componente animado reutilizable para mostrar cuando el fetch no devuelve resultados

---
+++ src/components/PaginaDetalle/DetalleAnime/MainSection/ListadoSinResultados.tsx
@@ -0,0 +1,28 @@
+import { motion } from "framer-motion";
+import {
+  transition,
+  varianteDetalleSecciones,
+} from "../../../../VariantesFramerMotion";
+
+type Props = {
+  nombreSeccion: string;
+};
+
+const ListadoSinResultados = ({ nombreSeccion }: Props) => {
+  return (
+    <motion.div
+      variants={varianteDetalleSecciones}
+      initial="initialSection"
+      animate="animateSection"
+      exit="exitSection"
+      transition={transition}
+      className="h-[100px] flex items-center justify-center"
+    >
+      <p className="uppercase text-sm font-bold lg:text-lg">
+        no se encontraron {nombreSeccion}
+      </p>
+    </motion.div>
+  );
+};
+
+export default ListadoSinResultados;
