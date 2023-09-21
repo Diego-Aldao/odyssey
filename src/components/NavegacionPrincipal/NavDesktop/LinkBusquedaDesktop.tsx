3f7ec38d38bc27a91f5e1dd2279adef1f639799c
Diego Aldao <diego_ind@live.com.ar>
2023-09-21 12:11:09 -0300

    creado el componente para el boton de busqueda de la pagina desktop

---
+++ src/components/NavegacionPrincipal/NavDesktop/LinkBusquedaDesktop.tsx
@@ -0,0 +1,41 @@
+import { motion } from "framer-motion";
+import { Icon } from "@iconify/react";
+import {
+  variantesNavegaciones,
+  variantesNavegacionesLinea,
+  transition,
+} from "../../../VariantesFramerMotion";
+
+type Props = {
+  handleClick: () => void;
+};
+
+const LinkBusquedaDesktop = ({ handleClick }: Props) => {
+  return (
+    <motion.li
+      variants={variantesNavegaciones}
+      whileHover="whileHover"
+      initial="initial"
+      animate="animate"
+      transition={transition}
+      onClick={handleClick}
+      className="h-full rounded-sm relative px-1 lg:px-2 xl:px-3 overflow-hidden flex flex-col items-center justify-center cursor-pointer"
+    >
+      <p className="flex gap-2 items-center mb-1">
+        <span className="hidden xl:block capitalize text-[13px] lg:text-lg">
+          buscar{" "}
+        </span>
+        <Icon icon="mingcute:search-3-line" className="h-4 w-4 lg:h-5 lg:w-5" />
+      </p>
+      <div className="animacion  w-full h-[2px] flex gap-1 lg:gap-2">
+        <motion.div
+          variants={variantesNavegacionesLinea}
+          transition={transition}
+          className="linea bg-main-color-background w-full h-full relative right-0"
+        ></motion.div>
+      </div>
+    </motion.li>
+  );
+};
+
+export default LinkBusquedaDesktop;
