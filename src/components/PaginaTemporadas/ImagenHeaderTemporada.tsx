3d28906168518c81d83e01d2e967c3055a3e8c7c
Diego Aldao <diego_ind@live.com.ar>
2023-10-07 15:20:10 -0300

    corregidos los tamaños para que respeten el padding x y para que la imagen no interfiera con el contenido de la pagina

--- src/components/PaginaTemporadas/ImagenHeaderTemporada.tsx
+++ src/components/PaginaTemporadas/ImagenHeaderTemporada.tsx
@@ -4,7 +4,7 @@ import ImagenHeaderMotion from "../FramerMotion/ImagenHeaderMotion";
 const ImagenHeaderTemporada = () => {
   return (
     <ImagenHeaderMotion>
-      <div className="absolute hidden md:block -right-6 md:-top-10 lg:-top-14 lg:-right-10 md:min-h-[45vw] h-full overflow-hidden xl:min-h-[550px] ">
+      <div className="absolute hidden md:block -right-0 md:-top-10 lg:-top-14  md:min-h-[45vw] h-full overflow-hidden xl:min-h-[550px] ">
         <img src={imagenTemporada} alt="" className="object-fill" />
       </div>
     </ImagenHeaderMotion>
