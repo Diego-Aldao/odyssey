9a2d9863ae5eeb81161bd17653d43ccd24d66753
Diego Aldao <diego_ind@live.com.ar>
2023-10-08 20:33:55 -0300

    añadida una altura minima para cuando la imagen viene mas chica que el estandar

--- src/components/PaginaDetalle/ImagenBackground.tsx
+++ src/components/PaginaDetalle/ImagenBackground.tsx
@@ -7,7 +7,7 @@ const ImagenBackground: React.FC<Props> = ({ imagenUrl }) => {
     <div className="px-1 w-full absolute md:bg-gradient-to-b md:from-main-black pt-1 rounded-t-xl left-0 top-0">
       <div className="relative imagen-detalle max-h-[500px] md:max-h-[400px] overflow-hidden left-0 top-0 rounded-t-xl w-full">
         <img
-          className="relative sm:-top-40 md:-top-60 lg:-top-80 xl:-top-96"
+          className="relative sm:-top-40 min-h-[400px] md:-top-60 lg:-top-80 xl:-top-96"
           src={imagenUrl}
           alt="imagen banner de anime"
         />
